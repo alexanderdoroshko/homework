862e9c76e9c9a09a2b55ee5f5e801985754ba037
alexanderdoroshko <hangman2311@gmail.com>
2021-04-15 21:32:57 +0300

    Shop and Product

---
+++ src/hw_16042021/ProductComparatorDecrease.java
@@ -0,0 +1,15 @@
+package hw_16042021;
+
+import java.util.Comparator;
+
+public class ProductComparatorDecrease implements Comparator<Product> {
+    @Override
+    public int compare(Product p1, Product p2) {
+        if (p1.getPrice() > p2.getPrice()) {
+            return -1;
+        } else if (p1.getPrice() < p2.getPrice()) {
+            return 1;
+        } else
+            return 0;
+    }
+}
---
+++ src/hw_16042021/Shop.java
@@ -0,0 +1,87 @@
+package hw_16042021;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Shop {
+    private ArrayList<Product> productList;
+
+
+    public Shop(ArrayList<Product> productList) {
+        this.productList = productList;
+    }
+
+    public Shop() {
+
+    }
+
+    public ArrayList<Product> getProductList() {
+        return productList;
+    }
+
+    public void addProduct(Product product) {
+        boolean contain = false;
+        for (Product p : productList) {
+            if (p.getId() == product.getId()) {
+                contain = true;
+                break;
+            }
+        }
+        if (!contain) {
+            productList.add(product);
+        } else {
+            System.out.println("Товар с таким id уже находится в магазине");
+        }
+    }
+
+    public void getAllProduct() {
+        System.out.println(productList.toString());
+    }
+
+
+    public void removeProduct(int id) {
+        boolean contain = false;
+        for (Product p : productList) {
+            if (p.getId() == id) {
+                contain = true;
+                productList.remove(p);
+                break;
+            }
+        }
+        if (contain) {
+            System.out.println("Товар удален из магазина");
+        } else {
+            System.out.println("Товара с таким id в магазине нет");
+        }
+    }
+
+    public void correctedProduct(int id) {
+        boolean contain = false;
+        for (Product p : productList) {
+            if (p.getId() == id) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Введите новое название товара");
+                String name = sc.nextLine();
+                System.out.println("Введите новую цену товара");
+                int price = sc.nextInt();
+                Product p1 = new Product(id, name, price);
+                productList.set(productList.indexOf(p), p1);
+                contain = true;
+            }
+        }
+        if (contain) {
+            System.out.println("Замена проведена успешно");
+        } else {
+            System.out.println("Товара с таким id в магазине нет");
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "productList=" + productList +
+                '}';
+    }
+}
